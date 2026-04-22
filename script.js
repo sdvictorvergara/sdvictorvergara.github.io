2fe5870f8ba121a4cf6541ab8999fcb30af64bfd
sdvictorvergara <sdvictorvergara@gmail.com>
2026-04-22 10:23:50 +0100

    Working on the technologies container menu

---
+++ script.js
@@ -0,0 +1,22 @@
+const lenguajeLvl = [
+  {
+    name: "python",
+    icon: "./assets/icons/python.svg",
+  },
+  {
+    name: "java",
+    icon: "./assets/icons/java.svg",
+  },
+  {
+    name: "html",
+    icon: "./assets/icons/html.svg",
+  },
+  {
+    name: "css",
+    icon: "./assets/icons/css.svg",
+  },
+  {
+    name: "javascript",
+    icon: "./assets/icons/javascript.svg",
+  },
+];
