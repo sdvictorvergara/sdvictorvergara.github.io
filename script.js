983c69e6c2347200bb078c35bbe38a3611e8cfd4
sdvictorvergara <sdvictorvergara@gmail.com>
2026-05-06 00:01:30 +0100

    additional work experience

--- script.js
+++ script.js
@@ -1,44 +1,56 @@
-const lenguajeLvl = [
-  {
-    name: "Python",
-    icon: "./assets/icons/python.svg",
-    lvl: "11%"
-  },
-  {
-    name: "Java",
-    icon: "./assets/icons/java.svg",
-    lvl: "15%"
-  },
-  {
-    name: "HTML",
-    icon: "./assets/icons/html.svg",
-    lvl: "20%"
-  },
-  {
-    name: "CSS",
-    icon: "./assets/icons/css.svg",
-    lvl: "18%"
-  },
-  {
-    name: "JavaScript",
-    icon: "./assets/icons/javascript.svg",
-    lvl: "5%"
-  },
-];
-
 const container = document.getElementById("skillsContainer");
 
-lenguajeLvl.forEach(lang => {
-  const skill = document.createElement("div");
-  skill.classList.add("skill");
-  skill.innerHTML = `
-    <div class="skillHeader">
-      <img src="${lang.icon}" alt="${lang.name}">
-      <span>${lang.name}</span>
-    </div>
-    <div class="bar">
-      <div class="progress" style="width: ${lang.lvl};"></div>
-    </div>
-  `;
-  container.appendChild(skill);
-});
+fetch("./assets/data/languagelvl.json").then(response => {
+
+  if (!response.ok) {
+    throw new Error("No se pudo cargar el archivo languagelvl.json");
+  }
+  return response.json();
+})
+
+  .then(languagelvl => {
+    languagelvl.forEach(lang => {
+      const skill = document.createElement("div");
+      skill.classList.add("skill");
+      skill.innerHTML = `
+        <div class="skillHeader">
+          <img src="${lang.icon}" alt="${lang.name}">
+          <span>${lang.name}</span>
+        </div>
+        <div class="bar">
+          <div class="progress" style="width: ${lang.lvl};"></div>
+        </div>
+      `;
+      container.appendChild(skill);
+    });
+  })
+  .catch(error => {
+    console.error("Error:", error);
+  });
+
+const container2 = document.getElementById("enterpriseExperience");
+
+fetch("./assets/data/enterpriseExperience.json").then(response => {
+
+  if (!response.ok) {
+    throw new Error("No se pudo cargar el archivo enterpriseExperience.json");
+  }
+  return response.json();
+})
+
+  .then(enterpriseExperience => {
+
+    enterpriseExperience.forEach(enterprise => {
+      const experience = document.createElement("div");
+      experience.classList.add("experience");
+      experience.innerHTML = `
+        <span>${enterprise.name}</span>
+        <p>${enterprise.date}</p>
+        <p>${enterprise.description}</p>
+    `;
+      container2.appendChild(experience);
+    });
+  })
+  .catch(error => {
+    console.error("Error:", error);
+  });
